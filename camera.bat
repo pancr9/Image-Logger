ca6fa8477db8319604ae7e05fa92383fd1206591
Rekhansh Panchal <rpanchal@uncc.edu>
2016-11-21 01:51:34 -0500

    Create camera.bat

---
+++ camera.bat
@@ -0,0 +1,4 @@
+REM This batch file is used to run Python Script via Command Window.
+REM pythonw is the program linked to pythonw.exe via environment variables.
+REM The link after pythonw should include actual link to the Python program which needs to be run.
+pythonw C:\Users\Rekhansh\AppData\Local\Programs\Python\Python35-32\camera.py
